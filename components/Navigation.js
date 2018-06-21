668ab8d7971f8e644deddc694778396cad595930
OsayamenOsaretin <osaretinosayamen@outlook.com>
2018-06-21 17:31:17 +0100

    add navigation component
    change chat icon

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,6 +5,16 @@ import { createMaterialTopTabNavigator } from 'react-navigation';
 import EventListContainer from './EventListContainer';
 
 
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
 class SettingsScreen extends Component {
   render() {
     return (
