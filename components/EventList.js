34c0564e3750d20b9ae72a3ee8d02feb0cdf4ab9
OsayamenOsaretin <osaretinosayamen@outlook.com>
2018-06-28 17:13:31 +0100

    add add button

--- components/EventList.js
+++ components/EventList.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react'
-import { FlatList, View } from 'react-native'
-import EventItem from './EventItem'
+import React, { Component } from 'react';
+import { FlatList, View, StyleSheet, Dimensions } from 'react-native';
+import EventItem from './EventItem';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 
 const EventList = ({ eventData, selectHandler }) => {
-    const selectHandler = () => alert("select handler clicked")
     return(
         <View>
             <FlatList
-              data={eventData}
-              renderItem={({ item }) => {
-                  return <EventItem 
-                            title={item.title} 
-                            date={item.date} 
-                            selectHandler={selectHandler} />
-                        }}
+                data={eventData}
+                renderItem={({ item }) => {
+                    return <EventItem 
+                        title={item.title} 
+                        date={item.date} 
+                        selectHandler={selectHandler} />
+                }}
             />
+            <View style={styles.addButton}>
+                <MaterialIcons 
+                    name={"add-circle"}
+                    size={50}
+                    color={'#983B59'}
+                />
+            </View>
         </View>)
 }
 
 export default EventList;
+
+const styles = StyleSheet.create({
+    addButton: {
+        position: 'absolute',
+        left: Dimensions.get('window').width - 70,
+        top: Dimensions.get('window').height - 150,
+        zIndex: 100,
+    }
+})
