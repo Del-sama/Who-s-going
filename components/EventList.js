8e6cca9deb3bc1dbb6a35b2837d31ec7bf4b6f29
OsayamenOsaretin <osaretinosayamen@outlook.com>
2018-06-28 17:58:06 +0100

    change header text color
    add navigation component
    
    change chat icon

--- components/EventList.js
+++ components/EventList.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     addButton: {
         position: 'absolute',
         left: Dimensions.get('window').width - 70,
-        top: Dimensions.get('window').height - 150,
+        top: Dimensions.get('window').height - 250,
         zIndex: 100},
     listContainer: {
         alignItems: 'center'
