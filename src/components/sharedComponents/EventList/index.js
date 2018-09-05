9377d6c627b841478cea037e32a3b4881718b10e
Del_sama <delores.diei@andela.com>
2018-09-05 11:27:57 +0100

    fix lint errors

--- src/components/sharedComponents/EventList/index.js
+++ src/components/sharedComponents/EventList/index.js
@@ -6,7 +6,13 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import EventItem from '../EventItem';
 import styles from './styles';
 
-const EventList = ({ eventData, selectHandler }) => (
+const eventData = [
+  { title: 'Movie at Imax', date: '1/2/2018' },
+  { title: 'Movie at genesis', date: '1/2/2018' },
+];
+
+
+const EventList = () => (
   <View style={styles.listContainer}>
     <FlatList
       data={eventData}
@@ -14,18 +20,10 @@ const EventList = ({ eventData, selectHandler }) => (
         <EventItem
           title={item.title}
           date={item.date}
-          selectHandler={selectHandler}
         />
       )}
       keyExtractor={(item, index) => `${index}`}
     />
-    <View style={styles.addButton}>
-      <MaterialIcons
-        name="add-circle"
-        size={50}
-        color="#983B59"
-      />
-    </View>
   </View>);
 
 export default EventList;
